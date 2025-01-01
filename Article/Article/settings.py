88669544832d1183fc10d9c29baf7a420c3bbc25
sartajahamad <sartaj1aha2@gmail.com>
2025-01-01 12:36:52 +0530

    Updated project with new changes

--- Article/Article/settings.py
+++ Article/Article/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'Article.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
