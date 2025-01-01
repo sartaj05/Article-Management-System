95359b168960eba58da396fdf8ae5b6734d8258c
sartajahamad <sartaj1aha2@gmail.com>
2025-01-01 12:23:38 +0530

    Updated project with new changes

--- Article/Article/wsgi.py
+++ Article/Article/wsgi.py
@@ -8,11 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Article.settings')
-
 application = get_wsgi_application()
+
 
 app=application
--- Article/build_files.sh
+++ Article/build_files.sh
@@ -1,8 +1,4 @@
 echo "BUILD START"
-# Ensure the correct Python version
-python3.11 -m pip install -r requirements.txt
-
-# Collect static files
+python3.11 -m pip install -r requirements.txt  # Ensure correct Python version
 python3.11 manage.py collectstatic --noinput --clear
-
 echo "BUILD END"
